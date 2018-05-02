fe4e4d8de8934d7f569bc25dbaf2acc308322a46
cthuluhoop123 <dowzhong@gmail.com>
2018-05-02 20:04:30 +1000

    Layed some basic ground.
    Added bot.js and removed ping.js.
    
    All our credentials are required from credentials.json, and the current prefix the bot uses is `;`

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const prefix = ';'
+
+const chalk = require('chalk')
+
+const credentials = require('./credentials.json')
+
+client.on('ready', () => console.log(chalk.green('Ready')))
+
+client.on('message', message => {
+  //handle command
+})
+
+client.login(credentials.token)
